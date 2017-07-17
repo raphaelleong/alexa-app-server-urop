57c4b0ad41b785a11ac48b3b7799046315397f64
Raphael Leong <raphaelleong@github.com>
2017-07-17 20:24:50 +0100

    updated analytics

--- analytics/alexa_apps.py
+++ analytics/alexa_apps.py
@@ -1,4 +1,6 @@
-import json, requests
+#!/usr/bin/python
+
+import json, requests, git, subprocess
 
 url = 'https://api.github.com/search/repositories?q=alexa+language:js'
 
@@ -6,4 +8,15 @@ resp = requests.get(url=url)
 data = json.loads(resp.text)
 result = json.dumps(data, indent=4)
 
-print(result)
+length = len(data['items']);
+
+for x in range(0,length):
+    name = data['items'][x]['html_url']
+    dirname = "demo/" + name[19:]
+    print(dirname)
+    subprocess.call(['mkdir', '-p', dirname]);
+    #subprocess.call(['git', 'clone', name, dirname]);
+
+
+#TODO get more repositories than 30
+#     process greps to perform analytics and update table
