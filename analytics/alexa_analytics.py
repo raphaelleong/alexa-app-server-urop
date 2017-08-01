3f481bb92e6bf1c31ed9298899c68acc34ffd57f
Raphael Leong <raphaelleong@github.com>
2017-08-01 01:59:31 +0100

    analytics

--- analytics/alexa_analytics.py
+++ analytics/alexa_analytics.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python
 
-import subprocess, os, glob
+import subprocess, os, glob, string
 
 def check_alexa_app(path):
-   # indexFile = path + "/index.js"
-   # if (os.path.isfile(indexFile)):
-   #     if 'require(\'alexa-app\')' in open(indexFile, encoding="utf8").read():
-   #         return (path + ": is an alexa-app", 1)
-
-   # indexFile = path + "/src/index.js"
-   # if (os.path.isfile(indexFile)):
-   #     if 'require(\'alexa-app\')' in open(indexFile, encoding="utf8").read():
-   #         return (path + ": is an alexa-app", 1)
-   # return (path + ": not an alexa-app", 0)
-   #
     term = "require(\'alexa-app\')"
     if (os.path.isdir(path)):
 
@@ -32,17 +21,6 @@ def check_alexa_app(path):
     return(path + ": is not an alexa-app", 0)
 
 def check_alexa_sdk(path):
-   # indexFile = path + "/index.js"
-   # if (os.path.isfile(indexFile)):
-   #     if 'require(\'alexa-app\')' in open(indexFile, encoding="utf8").read():
-   #         return (path + ": is an alexa-app", 1)
-
-   # indexFile = path + "/src/index.js"
-   # if (os.path.isfile(indexFile)):
-   #     if 'require(\'alexa-app\')' in open(indexFile, encoding="utf8").read():
-   #         return (path + ": is an alexa-app", 1)
-   # return (path + ": not an alexa-app", 0)
-   #
     term = "require(\'alexa-sdk\')"
     if (os.path.isdir(path)):
 
@@ -60,6 +38,69 @@ def check_alexa_sdk(path):
         os.chdir("../../../")
     return(path + ": is not an alexa-sdk", 0)
 
-def intent_analytics(repo):
-    subprocess.call(['./analyse.sh'])
+def find_intents(path):
+    file = path + "/index.js"
+    
+    alexa = "require(\'alexa-app\')"
+    app = ""
+    flag = 0
+
+    lines = list(skip_comments(file))
+
+    for line in lines:
+        if (flag < 1 and alexa in line.replace(" ","")):
+            lineSplit = line.split(" ")
+            alexaApp = lineSplit[1] + ".app"
+            flag = 1
+        
+        if (flag > 0 and alexaApp in line.replace(" ","")):
+            lineSplit = line.split(" ")
+            app = lineSplit[1]
+
+    count=0
+    term = app + ".intent"
+
+    for line in lines:
+        if term in line:
+            count = count + 1
+    print(count)
+    return count 
+
+#def find_destinations(path):
+
+#def find_slots(path):
+
+def find_module_sinks(path):
+    term = "require(\'alexa-sdk\')"
+    if (os.path.isdir(path)):
+
+        os.chdir(path)
+        for file in glob.glob( "*.js" ):
+                try: 
+                    f = open(file)
+                    contents = f.read()
+                    if term in contents:
+                        os.chdir("../../../")
+                        return(path + ": is an alexa-sdk", 1)
+                except: 
+                    contents = ""
+                    print("Could not print file: " + file + "\n", end=' ', flush=True)
+        os.chdir("../../../")
+    return(path + ": is not an alexa-sdk", 0)
+
+def skip_comments(filename):
+    blockStart = "/*"
+    blockEnd = "*/"
+    flag = 0
+    with open(filename) as f:
+        for line in f:
+            if flag < 1 and blockStart in line:
+                flag = 1
+
+            if flag < 1:
+                removeComment = line.split("//")
+                yield removeComment[0]
+
+            if flag > 0 and blockEnd in line:
+                flag = 0
 
--- analytics/analyse.sh
+++ analytics/analyse.sh
@@ -10,21 +10,30 @@ echo "Repo, URL, size (KB), intent functions"$'\r' >> test.txt
 for f in demo-app/*; do
   for g in $f/*; do
 
-    if [ -f $g/index.js ]; then
-    	echo "${g:9} analytics:"
-	res=`grep -ro --exclude-dir=node_modules .intent $g | wc -l`
+    if [ -f $g/index.js ]
+    then
+	
+	
+	intent=`py -c "import alexa_analytics as a; a.find_intents(\"$g\")"`
+	
+	if [ "$intent" == 0 ]
+	then
+		miss=$(($miss+1))
+		continue
+		#intent=`grep -ro --exclude-dir=node_modules .intent $g | wc -l`
+	fi
 	   
 	url="https://github.com/${g:9}"
 	size=`du -shk $g --exclude=$g/.git --exclude=$g/node_modules`
 	sizeRes=($size)
 
 	#slots="grep -zPo "'slots':( |)(\{([^{}]++|(?1))*\})" $g/index.js"
-
 	#echo $slots
+	echo "${g:9} analytics:"
 	echo "Repo size: ${sizeRes[0]}KB"
-	echo "$res intent function sinks"
+	echo "$intent intent functions"
 
-	echo "${g:9}, $url, ${sizeRes[0]}, $res"$'\r' >> test.txt
+	echo "${g:9}, $url, ${sizeRes[0]}, $intent"$'\r' >> test.txt
 	echo "--------------------------------------------------------------"
 	   
 	count=$(($count+1))
@@ -33,10 +42,11 @@ for f in demo-app/*; do
 done
 
 echo "$count repos analysed."
+echo "$miss repos could not be analysed."
 
 #App.intent, const, https://, uri, Request, Request-promise, rp(options)
 
 
 #find slots
 #find destination URIs
-
+#key modules used as sinks: i.e. request, request-promise, http, etc.
