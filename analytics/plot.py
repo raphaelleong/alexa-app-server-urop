a51d0816558850cba3aa83df21e5ee08427bb955
Raphael Leong <raphaelleong@github.com>
2017-08-02 08:07:33 +0100

    add graphs

--- analytics/plot.py
+++ analytics/plot.py
@@ -5,13 +5,22 @@ def draw_hist(slots, sinks, intents):
     f1 = plt.figure(1)
     plt.hist(slots, bins=4)
     plt.title("Histogram to show no. of slots for Alexa Repos (" + str(len(slots)) + ")")
-    
+    plt.xlabel("No. of Slots")
+    plt.ylabel("Frequency Density")
+    plt.grid(True, which='both')
+
     f2 = plt.figure(2)
     plt.hist(sinks, bins='auto')
     plt.title("Histogram to show no. of sink calls for Alexa Repos (" + str(len(sinks)) + ")")
+    plt.xlabel("No. of sink module calls (request/request-promise)")
+    plt.ylabel("Frequency Density")
+    plt.grid(True, which='both')
    
     f3 = plt.figure(3)
     plt.hist(intents, bins='auto')
     plt.title("Histogram to show no. of intent functions for Alexa Repos (" + str(len(intents)) + ")")
-   
+    plt.xlabel("No. of intent functions")
+    plt.ylabel("Frequency Density")
+    plt.grid(True, which='both')
+
     plt.show()
